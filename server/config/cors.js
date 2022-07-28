d50ea935212f0d03186a8733e2854b6346fc7b94
prasanth752 <prasanth752 c0796752@mylambton.ca>
2022-07-27 21:22:10 -0400

    directory-changes

--- server/config/cors.js
+++
@@ -1,6 +0,0 @@
-const corsOptions = {
-    origin: ["http://localhost:4000", "http://localhost:3000"],
-    optionsSuccessStatus: 200 // For legacy browser support
-}
-
-exports.corsOptions = corsOptions;
