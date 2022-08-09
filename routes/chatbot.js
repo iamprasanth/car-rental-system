7f0e14898ef3aac9b968554c7ff85c72716c7489
prasanth752 <prasanth752 c0796752@mylambton.ca>
2022-08-09 15:30:40 -0400

    changes

--- routes/chatbot.js
+++ routes/chatbot.js
@@ -2,7 +2,6 @@ const express = require('express');
 const chatbotRouter = express.Router();
 const chatbotController = require('../controllers/chatbot');
 
-// Register a new user
 chatbotRouter.route('/start-date').get(chatbotController.getServiceStartDate)
 
 chatbotRouter.route('/end-date').get(chatbotController.getServiceEndDate)
@@ -10,6 +9,5 @@ chatbotRouter.route('/end-date').get(chatbotController.getServiceEndDate)
 chatbotRouter.route('/total-fare').get(chatbotController.getTotalFare)
 
 chatbotRouter.route('/car-details').get(chatbotController.getCarDetails)
-
 
 module.exports = chatbotRouter
